d02f2035cee85c0d51950c05187530216b92014d
jrharrington1 <jrharrington1@stu.naperville203.org>
2017-05-04 10:30:19 -0500

    Started making methods for Board

--- Final Package/Board.java
+++ Final Package/Board.java
@@ -1,28 +1,60 @@
 
 
 /**
- * Write a description of class Board here.
+ * Each user gets a board. Each board can fire
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Jack Harrington, Phillip Sajaev, Jeevan Maddila
  */
 public class Board
 {
-    int[][] board = {{0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0}};
+    public int[][] board = {{0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0,0,0}};
     
-                     
-    public Board()
+    //0 = no battleship there   
+    //1 = battleship there
+    
+    public int bullets;
+    public String name; 
+    public Board(String name)
     {
+        this.name = name;
+        bullets = 5;
+    }
+    
+    public boolean fire(int c, int r)
+    {
+        bullets--;
+        boolean hit;
+        if (board[c][r] == 0)
+            hit = false;
+        else
+            hit = true;
+        return hit;
         
+        
+    }
+    
+    public void updateBoard(boolean hit, int c, int r)
+    {
+        if (hit)
+            //2 = hit
+            board[c][r] = 2;
+        else 
+            //1 = miss
+            board[c][r] = -1;
+    }
+    
+    public void viewBoard()
+    {
+        System.out.println(board);
     }
 
 }
